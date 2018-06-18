73cab7e4a857eeb1217516f4d7f043a815ee2217
ankurbrdwj <ankurbrdwj@gmail.com>
2018-06-18 21:34:25 +0530

    delet basic

--- AngularJsSamples/assets/js/mainSpec.js
+++
@@ -1,9 +0,0 @@
-describe ('filter', function () {
-beforeEach(module('myApp'));
-
-describe ('reverse' function(){
-it('it should reverse a string' , inject(function (reverseFilter){
-expect(reverseFilter('ABCD')).toEquals('DCBA');
-}))
-})
-})
